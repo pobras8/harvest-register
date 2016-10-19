94022bc4b0ad964f4f1e1b3ed5ad797fdb3bca28
Patrick Obras <pobras8@gmail.com>
2016-10-19 15:23:09 -0400

    guest form styling and template

--- src/app/guest-form/guest-form.component.ts
+++ src/app/guest-form/guest-form.component.ts
@@ -20,7 +20,7 @@ import { Router } from '@angular/router';
     ]
 })
 export class GuestFormComponent implements OnInit {
-
+  stateList: string[] = ["AK","AL","AR","AZ","CA","CO","CT","DC","DE","FL","GA","GU","HI","IA","ID","IL","IN","KS","KY","LA","MA","MD","ME","MH","MI","MN","MO","MS","MT","NC","ND","NE","NH","NJ","NM","NV","NY","OH","OK","OR","PA","PR","PW","RI","SC","SD","TN","TX","UT","VA","VI","VT","WA","WI","WV","WY"];
   constructor( private router: Router) { }
 
   ngOnInit() {
