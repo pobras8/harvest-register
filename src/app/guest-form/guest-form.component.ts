949db11b17c233acaf73309a1da2433509007e43
Patrick Obras <pobras8@gmail.com>
2016-10-19 15:52:11 -0400

    logo and style tweaks

--- src/app/guest-form/guest-form.component.ts
+++ src/app/guest-form/guest-form.component.ts
@@ -21,12 +21,23 @@ import { Router } from '@angular/router';
 })
 export class GuestFormComponent implements OnInit {
   stateList: string[] = ["AK","AL","AR","AZ","CA","CO","CT","DC","DE","FL","GA","GU","HI","IA","ID","IL","IN","KS","KY","LA","MA","MD","ME","MH","MI","MN","MO","MS","MT","NC","ND","NE","NH","NJ","NM","NV","NY","OH","OK","OR","PA","PR","PW","RI","SC","SD","TN","TX","UT","VA","VI","VT","WA","WI","WV","WY"];
-  constructor( private router: Router) { }
+  constructor( private router: Router ) { }
 
   ngOnInit() {
   }
 
+  /** Clears out data and returns to select screen
+  */
+  cancel() {
+    // TODO: Clear out locally saved form data
+    this.router.navigate(['/']);
+  }
+
+  /** Saves the new data to the DB then clears out local data. Returns to select screen on completion
+  */
   submit() {
+    // TODO: Call service to save data to DB
+    // TODO: Clear out locally saved form data
     this.router.navigate(['/']);
   }
 
