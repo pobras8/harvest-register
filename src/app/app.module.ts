563aa8d4287ac513d6962c8f91b7eb78d962d2fe
Patrick Obras <pobras8@gmail.com>
2016-10-25 16:11:10 -0400

    switched to reactive forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
@@ -20,7 +20,7 @@ import { SelectComponent } from './select/select.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    ReactiveFormsModule,
     HttpModule,
     routing
   ],
--- src/app/guest-form/guest-form.component.ts
+++ src/app/guest-form/guest-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, trigger, state, style, transition, animate } from '@angular/core';
+import { Validators, FormControl, FormGroup, FormArray, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { Person } from '../shared/models/person';
@@ -24,36 +25,80 @@ import { PrayerRequest } from '../shared/models/prayer-request';
     ]
 })
 export class GuestFormComponent implements OnInit {
-  stateList: string[] = ["IN","AK","AL","AR","AZ","CA","CO","CT","DC","DE","FL","GA","GU","HI","IA","ID","IL","KS","KY","LA","MA","MD","ME","MH","MI","MN","MO","MS","MT","NC","ND","NE","NH","NJ","NM","NV","NY","OH","OK","OR","PA","PR","PW","RI","SC","SD","TN","TX","UT","VA","VI","VT","WA","WI","WV","WY"];
-  newGuest: Person = new Person();
+  public stateList: string[] = ["IN","AK","AL","AR","AZ","CA","CO","CT","DC","DE","FL","GA","GU","HI","IA","ID","IL","KS","KY","LA","MA","MD","ME","MH","MI","MN","MO","MS","MT","NC","ND","NE","NH","NJ","NM","NV","NY","OH","OK","OR","PA","PR","PW","RI","SC","SD","TN","TX","UT","VA","VI","VT","WA","WI","WV","WY"];
+  public newGuest: Person = new Person();
+  public guestForm: FormGroup;
 
-  constructor( private router: Router ) { }
+  constructor(
+    private _router: Router,
+    private _fb: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.guestForm = new FormGroup({
+      firstName: new FormControl(''),
+      lastName: new FormControl(''),
+      emails: new FormArray([
+        new FormControl('')
+      ]),
+      phones: new FormArray([
+        new FormControl('')
+      ]),
+      address: new FormGroup({
+        line1: new FormControl(''),
+        city: new FormControl(''),
+        state: new FormControl('IN'),
+        zip: new FormControl('')
+      }),
+      marital: new FormControl(''),
+      ageCat: new FormControl(''),
+      reference: new FormControl(''),
+      nextSteps: new FormGroup({
+        party: new FormControl(''),
+        baptism: new FormControl(''),
+        newsletter: new FormControl(''),
+        discover: new FormControl(''),
+        serving: new FormControl('')
+      })
+    })
   }
 
-  addPhone() {
-      this.newGuest.phones.push('');
-  }
-
+  /** Pushes an empty control to the 'emails' form control array
+  */
   addEmail() {
-      this.newGuest.emails.push('');
+    const control = <FormArray>this.guestForm.controls['emails'];
+    control.push(new FormControl(''));
+  }
+
+  removeEmail(i: number) {
+    const control = <FormArray>this.guestForm.controls['emails'];
+    control.removeAt(i);
+  }
+
+  /** Pushes an empty control to the 'phones' form control array
+  */
+  addPhone() {
+    const control = <FormArray>this.guestForm.controls['phones'];
+    control.push(new FormControl(''));
+  }
+
+  removePhone(i: number) {
+    const control = <FormArray>this.guestForm.controls['phones'];
+    control.removeAt(i);
   }
 
   /** Clears out data and returns to select screen
   */
   cancel() {
-    // TODO: Clear out locally saved form data
-    this.router.navigate(['/']);
+    this._router.navigate(['/']);
   }
 
   /** Saves the new data to the DB then clears out local data. Returns to select screen on completion
   */
-  submit({ value, valid }: { value: any, valid: Boolean}) {
+  submit(formValues: Person) {
     // TODO: Call service to save data to DB
-    // TODO: Clear out locally saved form data
-    console.log(value, valid);
-    this.router.navigate(['/']);
+    console.log("Submitted form:",formValues);
+    this._router.navigate(['/']);
   }
 
 }
